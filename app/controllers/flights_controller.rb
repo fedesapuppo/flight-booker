29e0686249a3b004a8d81022d043032b10dfc8c8
Federico Sapuppo <fedesapuppo@hotmail.com>
2023-08-09 12:55:41 +0300

    move corresponding query logic to the Flight model

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,10 +2,10 @@ class FlightsController < ApplicationController
   def index
     @flight_search = FlightSearch.new(flight_search_params)
     @flights = Flight.all
-    @flights = @flights.where(departure_airport_id: @flight_search.departure_airport) if @flight_search.departure_airport.present?
-    @flights = @flights.where(arrival_airport_id: @flight_search.arrival_airport) if @flight_search.arrival_airport.present?
-    @flights = @flights.where(passenger_count: @flight_search.passenger_count) if @flight_search.passenger_count.present?
-    @flights = @flights.where("DATE(start_datetime) = ?", @flight_search.flight_date_datetime.to_date) if @flight_search.flight_date.present?
+    @flights = Flight.filter_by_departure_airport(@flight_search.departure_airport) if @flight_search.departure_airport.present?
+    @flights = Flight.filter_by_arrival_airport(@flight_search.arrival_airport) if @flight_search.arrival_airport.present?
+    @flights = Flight.filter_by_passangers(@flight_search.passenger_count) if @flight_search.passenger_count.present?
+    @flights = Flight.filter_by_date(@flight_search.flight_date_datetime) if @flight_search.flight_date.present?
   end
 
   def show
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -2,4 +2,20 @@ class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport", foreign_key: "departure_airport_id"
   belongs_to :arrival_airport, class_name: "Airport", foreign_key: "arrival_airport_id"
   belongs_to :user
+
+  def self.filter_by_departure_airport(airport_id)
+    where(departure_airport_id: airport_id)
+  end
+
+  def self.filter_by_arrival_airport(airport_id)
+    where(arrival_airport_id: airport_id)
+  end
+
+  def self.filter_by_passangers(passanger_count)
+    where(passanger_count: passanger_count)
+  end
+
+  def self.filter_by_date(date)
+    where(start_datetime: date.beginning_of_day..date.end_of_day)
+  end
 end
